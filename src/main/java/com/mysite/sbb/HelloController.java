464ab7cb5c25b30fe72639268a6503cef86008b8
rlaehdbs1805 <rlaehdbs1805@ajou.ac.kr>
2024-07-04 00:16:01 +0900

    0703

--- src/main/java/com/mysite/sbb/HelloController.java
+++ src/main/java/com/mysite/sbb/HelloController.java
@@ -3,7 +3,6 @@ package com.mysite.sbb;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
